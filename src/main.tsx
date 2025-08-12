4c44d8b1ffcf53b51d610154bab376fa1dfd8c41
Rob Meyer <rob.meyer@outreach.io>
2025-08-12 10:44:37 -0700

    initial checkpoint

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom/client';
+// import '@pigment-css/react/styles.css';
+// import './globals.css';
+// import App from './App';
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
+
+
+import { Container, createRoot } from "react-dom/client";
+import { App } from "./App";
+
+import '@pigment-css/react/styles.css';
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode as Container);
+root.render(<App />);
